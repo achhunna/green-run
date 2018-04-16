a229269f736396c470d2cb301e89ab7e4a2a1965
achhunna <achhunna@gmail.com>
2018-04-16 07:13:55 -0700

    added search

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,4 +10,15 @@ $(document).ready(function () {
     }
     $('#id').val(id);
   });
+
+  $('#searchBox').keyup(function () {
+    const filterValue = $(this).val().toLowerCase();
+    $('.beer-container').each(function () {
+      if ($(this).children('.name')[0].innerHTML.toLowerCase().indexOf(filterValue) > -1) {
+        $(this)[0].style.display = '';
+      } else {
+        $(this)[0].style.display = 'none';
+      }
+    });
+  });
 });
